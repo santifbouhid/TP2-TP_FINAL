39e39c42edcae07af70b3223dbee8f34226120b2
Santi Bouhid <santifbouhid@gmail.com>
2024-11-18 13:51:02 -0300

    Se modifica User Test

--- src/test/users.test.js
+++ src/test/users.test.js
@@ -8,22 +8,22 @@ const urlBase = supertest("http://localhost:8080")
 describe('Test de usuarios: ', () => {
     const data = generator.randomUser()
 
-    it('GET Users', async () => {
+    it('GET Users - admin', async () => {
         const response = await urlBase.get("/users/users")
         expect(response.status).to.equal(200)
     })
 
-    it('POST Users - admin', async () => {
-        const response = await urlBase.post("/users/newUser").set('rol', 'user').send(data)
+    it('GET Users - user', async () => {
+        const response = await urlBase.get("/users/users").set('role', 'user')
+        expect(response.status).to.equal(403)
+    })
+
+    it('POST Users ', async () => {
+        const response = await urlBase.post("/users/newUser").send(data)
         expect(response.status).to.equal(200)
     })
 
-    it('POST Users - user', async () => {
-        const response = await urlBase.post("/users/newUser").set('rol', 'user').send(data)
-        expect(response.status).to.equal(200)
-    })
-    
-    it('updateRestrictions Correct', async () => {
+    it('updateRestrictions OK', async () => {
         const response = await urlBase.patch("/users/update/restrictions/673ab0a512923e621784c85a").send(data)
         expect(response.status).to.equal(200)
     })
